18a307c9ca8543a4cabfaf27492ca8def016a771
Anderson Azevedo Grazina <andersongrazina@hotmail.com>
2020-10-13 11:48:40 -0300

    Iteração da Lista de Tarefas

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,6 +1,15 @@
 import { constants } from 'buffer';
 import React from 'react';
+import Lista from './Lista';
+import Navbar from './Navbar';
 
-const App = () => { };
+const App = () => {
+    return (
+        <div className="uk-container">
+            <Navbar></Navbar>
+            <Lista></Lista>
+        </div>
+    )
+};
 
 export default App;
---
+++ src/Models/ListaTodo.tsx
@@ -0,0 +1,7 @@
+export class ListaTodo {
+    constructor(
+        public id: number,
+        public title: string,
+        public done: boolean
+    ) { }
+}
